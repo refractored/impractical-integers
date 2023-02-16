b2b0ddc8b3e371d13563623a9a6a81e5fa55b54c
refractored <fatboy11905@gmail.com>
2023-02-16 11:06:59 -0600

    Initial Commit

---
+++ impracticalint/impracticalintApp.swift
@@ -0,0 +1,17 @@
+//
+//  impracticalintApp.swift
+//  impracticalint
+//
+//  Created by David M. Galvan on 2/16/23.
+//
+
+import SwiftUI
+
+@main
+struct impracticalintApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ impracticalintUITests/impracticalintUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  impracticalintUITestsLaunchTests.swift
+//  impracticalintUITests
+//
+//  Created by David M. Galvan on 2/16/23.
+//
+
+import XCTest
+
+final class impracticalintUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
