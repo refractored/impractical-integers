29e5d8acf314121e6b02629674e42869c2307301
David <me@refractored.net>
2023-07-29 18:28:33 -0500

    Overhaul main menu (Incomplete)

--- impracticalint/HomeScreen.swift
+++ impracticalint/HomeScreen.swift
@@ -12,47 +12,180 @@ struct equationInfo{
     var answer: Int
     var displayText: String
 }
-
+struct ImageOverlay: View{
+    var body: some View {
+        HStack{
+            Spacer()
+                .frame(width: 30)
+            VStack{
+                Spacer()
+                    .frame(height: 30)
+                ZStack{
+                    VStack{
+                        HStack{
+                            Text("Impractical Integers")
+                                .font(.title2)
+                                .fontWeight(.heavy)
+                                .foregroundColor(.white)
+                            //                            .padding(6)
+                                .font(.callout)
+                            Spacer()
+                        }
+                        HStack{
+                            Text("Master your math.")
+                                .font(.subheadline)
+                                .fontWeight(.semibold)
+                                .foregroundColor(.white)
+                            //                            .padding(6)
+                                .font(.callout)
+                                .multilineTextAlignment(.leading)
+                                .frame(alignment: .leading)
+                                Spacer()
+                        }
+                    }
+                }
+//                }.background(Color.black)
+//                    .opacity(0.8)
+//                    .cornerRadius(10.0)
+//                    .padding(6)
+            }
+        }
+    }
+}
+struct ScoreOverlay: View{
+    var body: some View {
+        HStack{
+            Spacer()
+                .frame(width: 60)
+            VStack{
+                Spacer()
+                    .frame(height: 30)
+                ZStack{
+                    VStack{
+                        Text("tes")
+                            .font(.title2)
+                            .fontWeight(.heavy)
+                            .foregroundColor(.white)
+                            .font(.callout)
+                        Spacer()
+                            .frame(maxHeight: 20)
+                    }
+                    .padding()
+                    .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 16.0))
+                
+                }
+//                }.background(Color.black)
+//                    .opacity(0.8)
+//                    .cornerRadius(10.0)
+//                    .padding(6)
+            }
+        }
+    }
+}
 struct HomeScreen: View {
     @State var timedNavigate = false
     @State var infiniteNavigate = false
     @State var leaderboardNavigate = false
+    let buttonBackground = Color("buttonBackground")
+    let buttonForeground = Color("buttonForeground")
     var body: some View {
-        VStack {
-            Image(systemName: "heart.fill")
-                .imageScale(.large)
-                .foregroundColor(.red)
-            Text("Hello Algebra!")
-            Divider()
-                .frame(width: 200)
-            Button("60 Seconds") {
-                timedNavigate = true
+        ZStack{
+            Color.pink.ignoresSafeArea()
+            VStack {
+                Image("math") // Replace `.building` with your image's name
+
+                    .resizable()
+                    .cornerRadius(20)
+                    .padding()
+                    //.scaledToFit()
+                    // .scaleEffect(0.5)
+                    .frame(width: 400, height: 500)
+                    .overlay(
+                        ImageOverlay(), alignment: .topLeading
+                    )
+                    .overlay(
+                        ScoreOverlay(), alignment: .bottomLeading
+                    )
+                //            Image(systemName: "heart.fill")
+                //                .imageScale(.large)
+                //                .foregroundColor(.red)
+                //            Text("Hello Algebra!")
+//                Divider()
+//                    .frame(width: 200)
+                Button(action:{
+                    timedNavigate = true
+                }) {
+                    HStack {
+                        Image(systemName: "clock.fill")
+                           .font(.title3)
+                        Text("Timed")
+
+
+//                            .fontWeight(.semibold)
+                            .font(.title3)
+                    }
+                    .frame(maxWidth: 320, maxHeight: 30)
+
+                }
+                
+                .foregroundColor(buttonForeground)
+                .buttonStyle(.borderedProminent)
+                .tint(buttonBackground)
+                
+//                Button("Infinite") {
+//                    infiniteNavigate = true
+//                }
+//                .foregroundColor(.blue)
+//                .buttonStyle(.borderedProminent)
+//                .tint(buttonBackground)
+                
+                Button(action:{
+                    infiniteNavigate = true
+                }) {
+                    HStack {
+                        Image(systemName: "infinity.circle.fill")
+                           .font(.title3)
+                        Text("Infinity")
+//                            .frame(maxWidth: 200, maxHeight: 30)
+//                            .fontWeight(.semibold)
+                            .font(.title3)
+                    }
+                    .frame(maxWidth: 320, maxHeight: 30)
+
+                }
+                .foregroundColor(buttonForeground)
+                .buttonStyle(.borderedProminent)
+                .tint(buttonBackground)
+//                Button("Leaderboard") {
+//                    leaderboardNavigate = true
+//                }
+//                .foregroundColor(.blue)
+//                .buttonStyle(.borderedProminent)
+//                .tint(buttonBackground)
+                Button(action:{
+                    leaderboardNavigate = true
+                }) {
+                    HStack {
+                        Image(systemName: "list.number")
+                           .font(.title3)
+                        Text("Leaderboard")
+//                            .frame(maxWidth: 200, maxHeight: 30)
+                        //                            .fontWeight(.semibold)
+                            .font(.title3)
+                    }
+                    .frame(maxWidth: 320, maxHeight: 30)
+
+                }
+                .foregroundColor(buttonForeground)
+                .buttonStyle(.borderedProminent)
+                .tint(buttonBackground)
             }
+            .navigate(to: TimedEquations(), when: $timedNavigate)
+            .navigate(to: InfiniteEquations(), when: $infiniteNavigate)
+            .navigate(to: ScoreboardView(), when: $leaderboardNavigate)
             
-            
-            .foregroundColor(.white)
-            .buttonStyle(.borderedProminent)
-            .tint(.red)
-            
-            Button("Infinite") {
-                infiniteNavigate = true
-            }
-            .foregroundColor(.white)
-            .buttonStyle(.borderedProminent)
-            .tint(.red)
-            Button("Leaderboard") {
-                leaderboardNavigate = true
-            }
-            .foregroundColor(.white)
-            .buttonStyle(.borderedProminent)
-            .tint(.red)
         }
-        .navigate(to: TimedEquations(), when: $timedNavigate)
-        .navigate(to: InfiniteEquations(), when: $infiniteNavigate)
-        .navigate(to: ScoreboardView(), when: $leaderboardNavigate)
-
-    
-
+        .accentColor(.black)
 
     }
 }
