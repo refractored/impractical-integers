60aa217bbee479534fb942376160c587f169ea34
refractored <me@refractored.net>
2023-08-02 19:41:18 -0500

    Major code refractoring for timedEquations

--- impracticalint/TimedEquations.swift
+++ impracticalint/TimedEquations.swift
@@ -23,109 +23,177 @@ extension AnyTransition {
 
 struct TimedEquations: View {
     @State var timeRemaining = 60
-    let countdown = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
-    @AppStorage("correct") private var timedHighScore = -1
     @State var sessionScore = 0
     @State var sliderValue: Float = 2.0
-    @State var ratioIC = "N/A"
     @State var attempts: Int = 0
-    @State var settings: Int = 0
     @State var equations = false
-    @Environment(\.presentationMode) var presentationMode
     @State var answer = ""
     @State var timer = -1
-    @State var text = "Begin"
-    let buttonBackground = Color("buttonBackground")
     @State var currentInfo = equationInfo(terms: [Int](), answer: 0, displayText: "")
+    @AppStorage("correct") private var timedHighScore = -1
+    @Environment(\.presentationMode) var presentationMode
+    let buttonBackground = Color("buttonBackground")
+
+
+
+    private func endGame(animated: Bool){
+        if animated{
+            withAnimation(.none) {
+                equations = false
+            }
+        }else{
+            equations = false
+        }
+        if sessionScore > timedHighScore {
+            timedHighScore = sessionScore
+        }
+    }
+    
+    private func startGame(){
+        equations = true
+        sessionScore = 0
+        currentInfo = equationShuffle(termCount: Int(sliderValue))
+        timeRemaining = 60
+    }
     var body: some View {
         
         
         VStack {
             
             if !equations{
-                Image(systemName: "clock.fill")
-                    .imageScale(.large)
-                    .foregroundColor(buttonBackground)
-                VStack {
-                    Text("Term Count:")
-                        .font(.headline)
-                    Text("\(Int(sliderValue))").font(.title2).fontWeight(.thin)
-                    Slider(value: $sliderValue, in: 2...5) {
-                    } minimumValueLabel: {
-                        Text("2").font(.callout).fontWeight(.thin)
-                    } maximumValueLabel: {
-                        Text("5").font(.callout).fontWeight(.thin)
-                    }
-                    .frame(width: 125, height: 5)
-                    
-                    if timedHighScore != -1{
-                        Text("High Score:")
-                        Text("\(timedHighScore)").font(.title2).fontWeight(.thin)
-                    }
-                    
-                }
+                MenuScreen(sliderValue: $sliderValue, timedHighScore: $timedHighScore, startGame: {
+                      self.startGame()
+                  })
             }
-            
             if equations {
-                VStack{
-                    Image(systemName: "clock.fill")
-                        .imageScale(.large)
-                        .foregroundColor(buttonBackground)
-                    Text(String(timeRemaining))
-                        .font(.largeTitle)
-                        .fontWeight(.heavy)
-                        .onReceive(countdown){ time in
-                            if timeRemaining > 0 {
-                                
-                                timeRemaining -= 1
-                            }else{
-                                if equations{
-                                    equations = false
-                                    text = "Begin"
-                                } else {
-                                    equations = true
-                                    text = "End"
-                                    // .foregroundColor(.red)
-                                }
-                                if sessionScore > timedHighScore{
-                                    timedHighScore = sessionScore
-                                }
-                            }
-                        }
-                    Text(currentInfo.displayText)
-                    TextField("Answer", text: $answer)
-                        .font(.subheadline)
-                        .fontWeight(.medium)
-                        .modifier(jiggleEffect(animatableData: CGFloat(attempts)))
-                        .multilineTextAlignment(.center)
-                        .foregroundColor(.white)
-                        .buttonStyle(.borderedProminent)
-                        .tint(buttonBackground)
-                }
-                .transition(.slideInFromBottom)
-                .animation(.spring())
+                GameScreen(
+                       timeRemaining: $timeRemaining,
+                       timedHighScore: $timedHighScore,
+                       answer: $answer,
+                       attempts: $attempts,
+                       currentInfo: $currentInfo,
+                   //    countdown: countdown,
+                       endGame: {
+                           self.endGame(animated: true)
+                       }
+                   )
             }
-            if !equations{
-                Button("\(text)") {
-                    equations = true
-                    sessionScore = 0
-                    currentInfo = equationShuffle(termCount: Int(sliderValue))
-                    timeRemaining = 60
+        }
+        if equations {
+            Keypad(answer: $answer, attempts: $attempts, endGame: {
+                  self.endGame(animated: true)
+              })
+        }
+        }
+    
+}
+
+struct KeyButton: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .frame(width: 75, height: 75)
+            .scaleEffect(configuration.isPressed ? 0.8 : 1.0)
+            .background(
+                RoundedRectangle(cornerRadius: 20, style: .circular)
+                    .foregroundColor(Color("buttonBackground"))
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 16)
+                            .stroke(Color("foregroundTwo"), lineWidth: 4)
+                    )
+            )
+            .foregroundColor(Color("foregroundTwo"))
+            .font(.title)
+            .fontWeight(.heavy)
+            .animation(.spring())
+    }
+}
+private struct GameScreen: View{
+    @Binding var timeRemaining: Int
+    @Binding var timedHighScore: Int
+    @Binding var answer: String
+    @Binding var attempts: Int
+    @Binding var currentInfo: equationInfo
+    private let countdown = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    var endGame: () -> Void
+    let buttonBackground = Color("buttonBackground")
+    @Environment(\.presentationMode) var presentationMode
+    var body: some View{
+        VStack{
+            Image(systemName: "clock.fill")
+                .imageScale(.large)
+                .foregroundColor(buttonBackground)
+            Text(String(timeRemaining))
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .onReceive(countdown) { _ in // Use countdown directly here
+                    if timeRemaining > 0 {
+                        timeRemaining -= 1
+                    } else {
+                        self.endGame()
+                    }
                 }
+            Text(currentInfo.displayText)
+            TextField("Answer", text: $answer)
+                .font(.subheadline)
+                .fontWeight(.medium)
+                .modifier(jiggleEffect(animatableData: CGFloat(attempts)))
+                .multilineTextAlignment(.center)
                 .foregroundColor(.white)
                 .buttonStyle(.borderedProminent)
                 .tint(buttonBackground)
+        }
+        .transition(.slideInFromBottom)
+        .animation(.spring())
+    }
+}
+private struct MenuScreen: View{
+    @Binding var sliderValue: Float
+    @Binding var timedHighScore: Int
+    var startGame: () -> Void
+    let buttonBackground = Color("buttonBackground")
+    @Environment(\.presentationMode) var presentationMode
+
+    var body: some View{
+        Image(systemName: "clock.fill")
+            .imageScale(.large)
+            .foregroundColor(buttonBackground)
+        VStack {
+            Text("Term Count:")
+                .font(.headline)
+            Text("\(Int(sliderValue))").font(.title2).fontWeight(.thin)
+            Slider(value: $sliderValue, in: 2...5) {
+            } minimumValueLabel: {
+                Text("2").font(.callout).fontWeight(.thin)
+            } maximumValueLabel: {
+                Text("5").font(.callout).fontWeight(.thin)
             }
-            if !equations{
-                Button("Back"){
-                    presentationMode.wrappedValue.dismiss()
-                }
-                .accentColor(buttonBackground)
-                
+            .frame(width: 125, height: 5)
+            
+            if timedHighScore != -1{
+                Text("High Score:")
+                Text("\(timedHighScore)").font(.title2).fontWeight(.thin)
             }
             
         }
-        if equations {
+        Button("Begin") {
+            self.startGame()
+        }
+        .foregroundColor(.white)
+        .buttonStyle(.borderedProminent)
+        .tint(buttonBackground)
+        Button("Back"){
+            presentationMode.wrappedValue.dismiss()
+        }
+        .accentColor(buttonBackground)
+        
+    }
+}
+struct Keypad: View{
+    @Binding var answer: String
+    @Binding var attempts: Int
+    var endGame: () -> Void
+    
+    var body: some View{
             VStack {
                 HStack {
                     Button(action: {
@@ -210,12 +278,7 @@ struct TimedEquations: View {
                     }
                     .buttonStyle(KeyButton())
                     Button(action: {
-                        withAnimation(.none) {
-                            equations = false
-                        }
-                        if sessionScore > timedHighScore {
-                            timedHighScore = sessionScore
-                        }
+                        self.endGame()
                     }) {
                         Text("End")
                     }
@@ -225,31 +288,9 @@ struct TimedEquations: View {
             }
             .transition(.slideInFromBottom)
             .animation(.none)
-        }
+    
     }
 }
-struct KeyButton: ButtonStyle {
-    func makeBody(configuration: Configuration) -> some View {
-        configuration.label
-            .frame(width: 75, height: 75)
-            .scaleEffect(configuration.isPressed ? 0.8 : 1.0)
-            .background(
-                RoundedRectangle(cornerRadius: 20, style: .circular)
-                    .foregroundColor(Color("buttonBackground"))
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 16)
-                            .stroke(Color("foregroundTwo"), lineWidth: 4)
-                    )
-            )
-            .foregroundColor(Color("foregroundTwo"))
-            .font(.title)
-            .fontWeight(.heavy)
-            .animation(.spring())
-    }
-}
-
-
-
 
 // Saving for later
 //                Button("Submit"){
